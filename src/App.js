bb16705ef58e3aced35259867c2873ec8cf2cc6b
viswa <saravanan.resume@gmail.com>
2023-12-16 20:54:48 +0800

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { count_triangles } from 'components/count_triangles'
+import { ThemeProvider } from 'styled-components'
+import { darkTheme } from 'theme'
+import { GlobalStyles } from 'global'
+import { binary_gap } from 'components/binary_gap'
+import { permutator } from 'components/permutator'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={{ ...darkTheme }}>
+      <>
+        <GlobalStyles />
+        <div className='App'>
+          <div>
+            count triangles
+            <ul>
+              <li>
+                {/* count_triangles([10, 2, 5, 1, 8, 20]) ={'>'}{' '}
+                {count_triangles([10, 2, 5, 1, 8, 20]).toString()} */}
+                {/* binary_gap(9) = {binary_gap(9)} */}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
